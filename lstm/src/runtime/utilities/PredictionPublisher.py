56db042c0e2cfc7167da0a9c228697a65122150c
jmarchel7bulls <jmarchel@7bulls.com>
2025-04-08 11:45:01 +0200

    fix typo in topic

--- lstm/src/runtime/utilities/PredictionPublisher.py
+++ lstm/src/runtime/utilities/PredictionPublisher.py
@@ -4,7 +4,7 @@ from exn.core.publisher import Publisher
 class PredictionPublisher(Publisher):
     metric_name = ""
     def __init__(self,application_name,metric_name):
-        super().__init__('publisher_'+application_name+'-'+metric_name, 'eu.nebulouscloud.preliminary_predicted.lstm.'+metric_name, True,True)
+        super().__init__('publisher_'+application_name+'-'+metric_name, 'eu.nebulouscloud.monitoring.preliminary_predicted.lstm.'+metric_name, True,True)
         self.metric_name = metric_name
 
     def send(self, body={}, application=""):
