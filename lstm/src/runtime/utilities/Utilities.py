d4e0d06275fb7d476db6e5440e3527164220b09e
jmarchel7bulls <jmarchel@7bulls.com>
2025-04-16 13:28:32 +0200

    adapt lstm soruce code to support env variables that override properties file

--- lstm/src/runtime/utilities/Utilities.py
+++ lstm/src/runtime/utilities/Utilities.py
@@ -1,15 +1,7 @@
-# Copyright (c) 2023 Institute of Communication and Computer Systems
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at https://mozilla.org/MPL/2.0/.        
-
-#from morphemic.dataset import DatasetMaker
 import datetime
-import logging,os
+import logging, os
 import json
 from influxdb_client import InfluxDBClient
-
 from runtime.operational_status.LstmPredictorState import LstmPredictorState
 
 
@@ -18,46 +10,104 @@ class Utilities:
     @staticmethod
     def print_with_time(x):
         now = datetime.datetime.now()
-        print("["+now.strftime('%Y-%m-%d %H:%M:%S')+"] "+str(x))
+        print("[" + now.strftime('%Y-%m-%d %H:%M:%S') + "] " + str(x))
+
+    @staticmethod
+    def get_config_value(env_var, config_key):
+        """
+        Returns the value of an environment variable if it is set;
+        otherwise returns the value from the configuration file.
+        """
+        env_val = os.getenv(env_var)
+        if env_val is not None:
+            logging.info(f"Overriding {config_key} with environment variable {env_var}: {env_val}")
+            return env_val
+        else:
+            return LstmPredictorState.configuration_details.get(config_key).data
 
     @staticmethod
     def load_configuration():
+        # First load configuration details from the properties file.
         with open(LstmPredictorState.configuration_file_location, 'rb') as config_file:
             LstmPredictorState.configuration_details.load(config_file)
-            LstmPredictorState.number_of_days_to_use_data_from = int(LstmPredictorState.configuration_details.get("number_of_days_to_use_data_from").data)
-            LstmPredictorState.prediction_processing_time_safety_margin_seconds = int(LstmPredictorState.configuration_details.get("prediction_processing_time_safety_margin_seconds").data)
-            LstmPredictorState.testing_prediction_functionality = LstmPredictorState.configuration_details.get("testing_prediction_functionality").data.lower() == "true"
-            LstmPredictorState.path_to_datasets = LstmPredictorState.configuration_details.get("path_to_datasets").data
-            LstmPredictorState.broker_address = LstmPredictorState.configuration_details.get("broker_address").data
-            LstmPredictorState.broker_port = int(LstmPredictorState.configuration_details.get("broker_port").data)
-            LstmPredictorState.broker_username = LstmPredictorState.configuration_details.get("broker_username").data
-            LstmPredictorState.broker_password = LstmPredictorState.configuration_details.get("broker_password").data
 
-            LstmPredictorState.influxdb_hostname = LstmPredictorState.configuration_details.get("INFLUXDB_HOSTNAME").data
-            LstmPredictorState.influxdb_port = int(LstmPredictorState.configuration_details.get("INFLUXDB_PORT").data)
-            LstmPredictorState.influxdb_username = LstmPredictorState.configuration_details.get("INFLUXDB_USERNAME").data
-            LstmPredictorState.influxdb_password = LstmPredictorState.configuration_details.get("INFLUXDB_PASSWORD").data
-            LstmPredictorState.influxdb_org = LstmPredictorState.configuration_details.get("INFLUXDB_ORG").data
+        # Now, override each value with an environment variable if available.
+        LstmPredictorState.number_of_days_to_use_data_from = int(
+            Utilities.get_config_value("NUMBER_OF_DAYS_TO_USE_DATA_FROM", "number_of_days_to_use_data_from")
+        )
 
-        #This method accesses influx db to retrieve the most recent metric values.
-            Utilities.print_with_time("The configuration effective currently is the following\n "+Utilities.get_fields_and_values(LstmPredictorState))
+        LstmPredictorState.prediction_processing_time_safety_margin_seconds = int(
+            Utilities.get_config_value("PREDICTION_PROCESSING_TIME_SAFETY_MARGIN_SECONDS", "prediction_processing_time_safety_margin_seconds")
+        )
+
+        LstmPredictorState.testing_prediction_functionality = Utilities.get_config_value(
+            "TESTING_PREDICTION_FUNCTIONALITY", "testing_prediction_functionality"
+        ).lower() == "true"
+
+        LstmPredictorState.path_to_datasets = Utilities.get_config_value(
+            "PATH_TO_DATASETS", "path_to_datasets"
+        )
+
+        LstmPredictorState.broker_address = Utilities.get_config_value(
+            "BROKER_ADDRESS", "broker_address"
+        )
+
+        LstmPredictorState.broker_port = int(
+            Utilities.get_config_value("BROKER_PORT", "broker_port")
+        )
+
+        LstmPredictorState.broker_username = Utilities.get_config_value(
+            "BROKER_USERNAME", "broker_username"
+        )
+
+        LstmPredictorState.broker_password = Utilities.get_config_value(
+            "BROKER_PASSWORD", "broker_password"
+        )
+
+        LstmPredictorState.influxdb_hostname = Utilities.get_config_value(
+            "INFLUXDB_HOSTNAME", "INFLUXDB_HOSTNAME"
+        )
+
+        LstmPredictorState.influxdb_port = int(
+            Utilities.get_config_value("INFLUXDB_PORT", "INFLUXDB_PORT")
+        )
+
+        LstmPredictorState.influxdb_username = Utilities.get_config_value(
+            "INFLUXDB_USERNAME", "INFLUXDB_USERNAME"
+        )
+
+        LstmPredictorState.influxdb_password = Utilities.get_config_value(
+            "INFLUXDB_PASSWORD", "INFLUXDB_PASSWORD"
+        )
+
+        LstmPredictorState.influxdb_org = Utilities.get_config_value(
+            "INFLUXDB_ORG", "INFLUXDB_ORG"
+        )
+
+        # Log the effective configuration.
+        Utilities.print_with_time("The configuration effective currently is the following\n" +
+                                    Utilities.get_fields_and_values(LstmPredictorState))
 
     @staticmethod
     def update_influxdb_organization_id():
-        client = InfluxDBClient(url="http://" + LstmPredictorState.influxdb_hostname + ":" + str(LstmPredictorState.influxdb_port), token=LstmPredictorState.influxdb_token)
+        client = InfluxDBClient(
+            url="http://" + LstmPredictorState.influxdb_hostname + ":" + str(LstmPredictorState.influxdb_port),
+            token=LstmPredictorState.influxdb_token
+        )
         org_api = client.organizations_api()
-        # List all organizations
+        # List all organizations.
         organizations = org_api.find_organizations()
 
-        # Find the organization by name and print its ID
+        # Find the organization by name and set its ID.
         for org in organizations:
             if org.name == LstmPredictorState.influxdb_organization:
                 logging.info(f"Organization Name: {org.name}, ID: {org.id}")
                 LstmPredictorState.influxdb_organization_id = org.id
                 break
+
     @staticmethod
     def fix_path_ending(path):
-        if (path[-1] is os.sep):
+        if path[-1] == os.sep:
             return path
         else:
             return path + os.sep
@@ -65,9 +115,9 @@ class Utilities:
     @staticmethod
     def default_to_string(obj):
         return str(obj)
-    @classmethod
-    def get_fields_and_values(cls,object):
-        #Returns those fields that do not start with __ (and their values)
-        fields_values = {key: value for key, value in object.__dict__.items() if not key.startswith("__")}
-        return json.dumps(fields_values,indent=4,default=cls.default_to_string)
 
+    @classmethod
+    def get_fields_and_values(cls, obj):
+        # Returns a JSON string with object fields (ignoring those starting with __) and their values.
+        fields_values = {key: value for key, value in obj.__dict__.items() if not key.startswith("__")}
+        return json.dumps(fields_values, indent=4, default=cls.default_to_string)
